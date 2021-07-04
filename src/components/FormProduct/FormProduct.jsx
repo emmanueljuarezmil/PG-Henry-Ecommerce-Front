9d78bed84c4433db3721b2a9ca275fe42426353c
sofiarocchietti <you@example.com>
2021-07-04 00:04:45 -0300

    adding-components

---
+++ src/components/FormProduct/FormProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FormProduct() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FormProduct
---
+++ src/components/FormPurchase/FormPurchase.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FormPurchase() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FormPurchase
