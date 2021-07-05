389c4e7466fb653dff1138d657aac1199c3946ee
Gustavitory <78607300+Gustavitory@users.noreply.github.com>
2021-07-05 16:18:33 -0400

    s7-crear-componente-search-bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Route} from 'react-router-dom';
 import React from 'react';
 
 import LandingPage from './components/Landing/Landing';
+import SearchBar from './components/SearchBar/SearchBar'
 
 function App() {
   return (
       <React.Fragment>
         <Route path='/' component={LandingPage}/>
+        <Route path='/' component={SearchBar}/>
       </React.Fragment>
   );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './SearchBar.css'
+
 
 function SearchBar() {
+    const [search,setSearch]=useState('')
+    
+    const handleChange=(e)=>{
+        setSearch(e.target.value)
+    }
     return (
-        <div>
-            
+        <div classname='cont'>
+            <form>
+                <input className='input-icon' type="text" value={search} onChange={handleChange} placeholder=" Search"/>
+            </form>
         </div>
     )
 }
