d75a00600bd39e3c1b0b67f0ea0b415060db0218
Emmanuel Juarez Milevich <emmanueljuarezmil@gmail.com>
2021-07-05 20:14:08 -0300

    Revert "s7-crear-componente-search-bar"

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,9 @@
-import React,{useState} from 'react'
-import './SearchBar.css'
-
+import React from 'react'
 
 function SearchBar() {
-    const [search,setSearch]=useState('')
-    
-    const handleChange=(e)=>{
-        setSearch(e.target.value)
-    }
     return (
-        <div classname='cont'>
-            <form>
-                <input className='input-icon' type="text" value={search} onChange={handleChange} placeholder=" Search"/>
-            </form>
+        <div>
+            
         </div>
     )
 }
