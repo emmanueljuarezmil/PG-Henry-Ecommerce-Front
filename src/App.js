4dc6b6edb920ec7c27b30e75db714fc7df22e983
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-07-06 01:19:26 -0300

    Formproducts inputs text y handlers completos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import LandingPage from './components/Landing/Landing';
 import ProductDetail from './components/ProductDetail/ProductDetail'
 import Home from './components/Home/Home';
+import FormProduct from './components/FormProduct/FormProduct';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/' component={LandingPage}/>
         <Route exact path='/home' component={Home}/>
         <Route exact path='/product/:id' component={ProductDetail}/>
+        <Route exact path='/admin/product' component={FormProduct}/>
       </React.Fragment>
   );
 }
--- src/components/FormProduct/FormProduct.jsx
+++ src/components/FormProduct/FormProduct.jsx
@@ -1,34 +1,49 @@
 import React, {useState, useRef} from 'react'
 import Select from 'react-select'
+const productsHardcoded = require('./DBproductsform.json')
 
 function FormProduct() {
-    // create, read, delete, update
-    // si cargo necesito todos los valores del nuevo producto
-    // si leo necesito hacer un get de todo lo del producto
-    // si borro necesito marcar el producto
-    // si updateo necesito los datos nuevos o cambiados
 
-    // cuestiones:
-
-    // si quiero crear un producto nuevo no es tanto viaje, lleno el form nomas y chau
-
-    // ahora, si quiero ver los productos para ver que borro edito o etc son 900, tengo que tener alguna forma practica de ver los productos con algunos datos no todos
-    // una vez que veo cual es el que quiero ver full, lo selecciono y ahi se cargan todos los datos de ese producto en el mismo form
-    // y ahi puedo decidir si editar algo de ese producto y updatear, o eliminarlo
+    // como admin poder
+    // 1) Cargar un producto nuevo
+    // 1)a) Form (name, photos (max 3), description, stock, selled, perc_desc, price, categories)
+    // 2) Ver facilmente todos los productos y buscar por nombre o categoria para acceder al detalle y asi modificarlos o eliminarlos
+    // 2)a) Ver todos los productos de una forma practia
+    // 2)b) Buscar productos por nombre
+    // 2)c) Buscar productos por categoria
+    // 2)d) Seleccionar un producto y (form)
+    //      Ver todos sus datos
+    //      Modificar sus datos
+    //      Borrarlo
+    //
+    // validar el input antes de mostrar boton de enviar y mostrar mensaje de error
+    // mensaje de confirmacion al querer crear actualizar eliminar
 
     const [actionType, setActionType] = useState(null)
+    const [input, setInput] = useState({})
     const actionOptions = [
         { value: 'create', label: 'Crear un nuevo producto' },
-        { value: 'readAllProducts', label: 'Ver los productos existentes, editarlos o eliminarlos' }
+        { value: 'readAndModified', label: 'Ver los productos existentes, editarlos o eliminarlos' }
     ]
-    const allProducts = [
-        { value: 'id producto 1', label: 'nombre producto 1' },
-        { value: 'id producto 2', label: 'nombre producto 2' }
-    ] //aca iria un map de una ruta nueva del back que traiga solamente nombre y id de los productos o algun dato mas pero que no sea full para que no sea tan pesado
+    // const allProducts = productsHardcoded.map(product => {
+    //     return {
+    //         value: product.id,
+    //         label: product.name
+    //     }
+    // })
+    // allProducts.unshift({value: '', label: ''})
+    //aca iria un map de una ruta nueva del back que traiga solamente nombre y id de los productos o algun dato mas pero que no sea full para que no sea tan pesado
     // una vez que seleccionas un producto, se pide todo el detalle al back y se carga en el input del formulario para ver si queres borrarlo, editarlo o no hacer nada
     
-    const actionOptionsRef = useRef(null)
-    
+    function handleChange(e) {
+        e.preventDefault()
+        const { value, name } = e.target;
+        setInput({
+            ...input,
+            [name]: value
+        });
+    }
+
     return (
         <div>
             <h2>Accion que deseas realizar:</h2>
@@ -36,20 +51,58 @@ function FormProduct() {
             onChange={(e) => setActionType(e.value)}>
             </Select>
             {
-                actionType !== 'create' ?
-                <Select options={allProducts}></Select> :
-                null
+                actionType === 'create' ?
+                <form action="">
+                    <div>
+                        <input type="text"
+                        name="name" 
+                        placeholder="Nombre del producto"
+                        value={input.name}
+                        onChange={handleChange}/> 
+                    </div>
+                    <div>
+                        <input type="text"
+                        name="description"
+                        placeholder="Descripcion del producto"
+                        value={input.description}
+                        onChange={handleChange}/> 
+                    </div>
+                    <div>
+                        <input type="number" 
+                        name="stock" 
+                        placeholder="Stock actual"
+                        value={input.stock}
+                        onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <input type="number"
+                        name="selled"
+                        placeholder="Cantidades vendidas"
+                        value={input.selled}
+                        onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <input type="number"
+                        name="price"
+                        placeholder="Precio del producto"
+                        value={input.price}
+                        onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <input type="number"
+                        name="perc_desc"
+                        placeholder="Porcentaje de descuento"
+                        value={input.perc_desc}
+                        onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <p>Drag and drop zone</p>
+                        <p>Examinar comun</p>
+                        <p>Imagenes ya cargadas</p>
+                    </div>
+                </form> :
+                <p>Lista de productos y barra de busqueda</p>
             }
-            <form action="">
-                <div>
-                    <label htmlFor=""></label>
-                    <input type="text" name="" id="" /> 
-                </div>
-                <div>
-                    <label htmlFor=""></label>
-                    <input type="text" name="" id="" /> 
-                </div>
-            </form>
             
         </div>
     )
