169517103f9e9e9fa8cc01acaf5756a5ff44f823
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-07-07 12:56:48 -0300

    add dropzone to formpr y package json (in proc)

--- src/components/FormProduct/FormProduct.jsx
+++ src/components/FormProduct/FormProduct.jsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react'
+import { div } from 'prelude-ls'
+import React, {useState, useCallback} from 'react'
+import Dropzone, {useDropzone} from 'react-dropzone'
 import Select from 'react-select'
 
 function FormProduct() {
-
+    
     // como admin poder
     // 1) cargar un producto nuevo
     // 1)a) form (name, photos (max 3), description, stock, selled, perc_desc, price, category)
@@ -18,33 +20,55 @@ function FormProduct() {
     // validar el input antes de mostrar boton de enviar y mostrar mensaje de error
     // mensaje de confirmacion al querer crear actualizar eliminar
 
-    const [actionType, setActionType] = useState(null)
-    const [input, setInput] = useState({})
-    const actionOptions = [
-        { value: 'create', label: 'Crear un nuevo producto' },
-        { value: 'readAndModified', label: 'Ver los productos existentes, editarlos o eliminarlos' }
-    ]
+        
     // const allProducts = productsHardcoded.map(product => {
-    //     return {
-    //         value: product.id,
-    //         label: product.name
-    //     }
-    // })
-    // allProducts.unshift({value: '', label: ''})
-    //aca iria un map de una ruta nueva del back que traiga solamente nombre y id de los productos o algun dato mas pero que no sea full para que no sea tan pesado
-    // una vez que seleccionas un producto, se pide todo el detalle al back y se carga en el input del formulario para ver si queres borrarlo, editarlo o no hacer nada
-    
-    function handleChange(e) {
-        e.preventDefault()
-        const { value, name } = e.target;
-        setInput({
-            ...input,
-            [name]: value
-        });
-    }
-
-    return (
-        <div>
+        //     return {
+            //         value: product.id,
+            //         label: product.name
+            //     }
+            // })
+            // allProducts.unshift({value: '', label: ''})
+            //aca iria un map de una ruta nueva del back que traiga solamente nombre y id de los productos o algun dato mas pero que no sea full para que no sea tan pesado
+            // una vez que seleccionas un producto, se pide todo el detalle al back y se carga en el input del formulario para ver si queres borrarlo, editarlo o no hacer nada
+            
+            // function onSumbit(e) {
+                //     e.preventDefault()
+                // }
+                
+                // async function uploadImage() {
+                    //     subir las imagenes del dropzone a una nube
+                    //     obtener el enlace 
+                    //     guardarlo en el state
+                    // }
+                    
+                    const [actionType, setActionType] = useState(null)
+                    const [input, setInput] = useState({})
+                    const actionOptions = [
+                        { value: 'create', label: 'Crear un nuevo producto' },
+                        { value: 'readAndModified', label: 'Ver los productos existentes, editarlos o eliminarlos' }
+                    ]
+                    
+                    function handleChange(e) {
+                        e.preventDefault()
+                        const { value, name } = e.target;
+                        setInput({
+                            ...input,
+                            [name]: value
+                        });
+                    }
+                    
+                    
+                    const onDrop = useCallback(async acceptedFiles => {
+                        
+                    }, [])
+                    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+                    // getRootProps
+                    // getInputProps
+                    // isDragActive
+                    const maxImageSize = 250000
+                    
+                    return (
+                        <div>
             <h2>Accion que deseas realizar:</h2>
             <Select options={actionOptions}
             onChange={(e) => setActionType(e.value)}>
@@ -96,9 +120,31 @@ function FormProduct() {
                         onChange={handleChange}/>
                     </div>
                     <div>
-                        <p>Drag and drop zone</p>
-                        <p>Examinar comun</p>
-                        <p>Imagenes ya cargadas</p>
+                        <Dropzone 
+                        onDrop={acceptedFiles => onDrop(acceptedFiles)}
+                        maxSize={maxImageSize}
+                        accept='image/*'>
+                            {({getRootProps, getInputProps}) => (
+                                <section>
+                                <div {...getRootProps()}>
+                                    <input {...getInputProps()} />
+                                    <p>Arrastra y suelta tus fotos aqui o haz click para cargar desde el explorador</p>
+                                </div>
+                                </section>
+                            )}
+                        </Dropzone>
+                        <div>
+                            <p>Fotos</p>
+                            {
+                                input.foto && input.foto.length ?
+                                input.foto.map(foto => (
+                                    <div>
+                                        <img src={foto} alt="Img not found" />
+                                    </div>
+                                )) :
+                                null
+                            }
+                        </div>
                     </div>
                 </form> :
                 // barra de busqueda y lista de productos existentes
