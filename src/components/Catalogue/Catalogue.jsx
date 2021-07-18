1a4b9b0f007a226b3b63097585d0576979c9f9fa
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-07-18 15:32:34 -0300

    clean console warning

--- src/components/Catalogue/Catalogue.jsx
+++ src/components/Catalogue/Catalogue.jsx
@@ -9,7 +9,7 @@ function Catalogue({products}) {
             <div className='catalogue_container'>
                 {
                     products && products.map((products, index) => (
-                        <ProductCard index={index} product={products} />
+                        <ProductCard index={index} key={index} product={products} />
                     ))
                 }
             </div>
