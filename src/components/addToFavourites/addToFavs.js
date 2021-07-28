e09d58450f544092a96c5c3c3b3034905c8615ab
Fernando Perez Diez <perezdiezf@gmail.com>
2021-07-28 20:19:45 -0300

    add to favourites button

--- src/components/addToFavourites/addToFavs.js
+++ src/components/addToFavourites/addToFavs.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from 'axios';
 import {url} from '../constantURL'
 import { headers } from "../../controllers/GetHeaders";
+import swal from 'sweetalert';
 
 
 
@@ -11,6 +12,13 @@ export function AddToFavs (props){
     
     async function handleChange(){
         const response = await axios.post(`${url}/users/favs`, {idProduct: props.product.id}, {headers})
+        swal({
+            icon: "success",
+            title: "Se agregó a favoritos!",
+            text: "  ",
+            button: null,
+            timer: 2000 
+        });
         console.log(response)
     }
 
